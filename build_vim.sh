1ebd5b29830e117a94e0a68d5a9a13bb39ec9256
wsdjeg <wsdjeg@outlook.com>
2016-02-01 01:04:34 +0800

    Update vimrc

---
+++ build_vim.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+./configure \
+    --with-features=huge \
+    --with-compiledby="wsdjeg" \
+    --enable-multibyte \
+    --enable-gui=gtk2 \
+    --enable-gpm \
+    --prefix=/usr \
+    --enable-cscope \
+    --enable-fontset \
+    --enable-sniff \
+    --enable-xim \
+    --enable-fail-if-missing \
+    --enable-mzschemeinterp \
+    --enable-perlinterp \
+    --enable-luainterp \
+    --enable-tclinterp\
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --with-python3-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu
