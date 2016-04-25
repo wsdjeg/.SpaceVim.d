7ec8b3ab5c8a5e77340b4363a76b6ea38a4bf7e1
wsdjeg <wsdjeg@outlook.com>
2016-04-26 07:31:35 +0800

    Fix sudo install

--- sudo_install.sh
+++ sudo_install.sh
@@ -1,3 +1,20 @@
 if [ ! -d "/etc/lightdm" ];then
-    sudo ln -s "$HOME/DotFiles/lightdm" "/etc/lightdm"
+    sudo mkdir /etc/lightdm
+    sudo cp -r "$HOME/DotFiles/lightdm" "/etc/lightdm"
+else
+    sudo rm -rf /etc/lightdm
+    sudo cp -r "$HOME/DotFiles/lightdm" "/etc/lightdm"
 fi
+
+if [ ! -d "/usr/share/wallpapers" ];then
+    sudo mkdir -p "/usr/share/wallpapers"
+    sudo cp "$HOME/DotFiles/backgrounds/security-wallpaper-hacker-art-securityhacker.jpg" "/usr/share/wallpapers/hacker.jpg"
+else
+    if [ ! -e "/usr/share/wallpapers/hacker.jpg" ];then
+        sudo cp "$HOME/DotFiles/backgrounds/security-wallpaper-hacker-art-securityhacker.jpg" "/usr/share/wallpapers/hacker.jpg"
+    else
+        sudo rm "/usr/share/wallpapers/hacker.jpg"
+        sudo cp "$HOME/DotFiles/backgrounds/security-wallpaper-hacker-art-securityhacker.jpg" "/usr/share/wallpapers/hacker.jpg"
+    fi
+fi
+
