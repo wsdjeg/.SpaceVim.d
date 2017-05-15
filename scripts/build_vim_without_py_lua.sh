3ea90c0c1ccca7a0db4f4c1a187bd9f0af328b5d
wsdjeg <wsdjeg@163.com>
2017-05-15 19:41:43 +0800

    Add basic build vim script

---
+++ scripts/build_vim_without_py_lua.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./configure \
+    --with-features=huge \
+    --with-compiledby="wsdjeg" \
+    --enable-multibyte \
+    --enable-gui=gtk2 \
+    --enable-gpm \
+    --prefix=/usr \
+    --enable-cscope \
+    --enable-fontset \
+    --enable-sniff \
+    --enable-xim \
+    --enable-fail-if-missing \
+    --enable-mzschemeinterp \
