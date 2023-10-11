cc7f99626e5886004f2ee7113e2019629a7c7e0b
wsdjeg <wsdjeg@outlook.com>
2023-10-11 19:21:03 +0800

    update

--- lua/encrypt.lua
+++ lua/encrypt.lua
@@ -4,30 +4,28 @@
 
 local M = {}
 
-local str = require('spacevim.api.data.string')
+local str = require("spacevim.api.data.string")
 
-local key = ''
+local key = 5
 
-local function encode(chars)
+local function encode(char)
+  return vim.fn.char2nr(char) + key
   
 end
 
 function M.encrypt()
-  local text = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
-  local chars = str.string2chars(text)
+	local text = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
+	local chars = str.string2chars(text)
+	local rst = {}
+	for _, c in ipairs(chars) do
+		table.insert(rst, encode(c))
+	end
 
-
-  
+	vim.api.nvim_buf_set_lines(0, 1, -1, false, rst)
 end
 
+function M.decrypt() end
 
-function M.decrypt()
-  
-end
-
-function M.setup(config)
-  
-end
-
+function M.setup(config) end
 
 return M
