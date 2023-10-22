15a564fca5310c35478c1f5f7ffc72dd0a90f364
wsdjeg <wsdjeg@outlook.com>
2023-10-22 10:56:17 +0800

    update keyboard

--- lua/baby/keyboard.lua
+++ lua/baby/keyboard.lua
@@ -4,26 +4,26 @@ local M = {}
 
 local quit = false
 
-local function open_win()
-  vim.cmd('tabnew')
-  vim.cmd('set buftype=nofile')
+local function insert_box(w)
+	local box_top = "+--------------------------------------+"
+	local box_m = "|                                      |"
+	local box_root = "+--------------------------------------+"
 
-  vim.api.nvim_buf_set_lines(0, 1, -1, false, {
-    '                     ----------------------              '
-  })
+	return { box_top, box_m, box_root }
 end
 
+local function open_win()
+	vim.cmd("tabnew")
+	vim.cmd("set buftype=nofile")
 
-local function insert_box()
-  
+	vim.api.nvim_buf_set_lines(0, 1, -1, false, insert_box())
 end
 
 function M.start()
-  open_win()
-  while quit do
-    --body
-  end
+	open_win()
+	while quit do
+		--body
+	end
 end
-
 
 return M
