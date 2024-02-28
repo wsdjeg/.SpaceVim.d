b512e803944de6da7a4e8c2755c0e9d9b2ae00bb
Eric Wong <eric@wsdjeg.net>
2024-02-28 13:27:03 +0800

    add viewer

---
+++ lua/rss/feed.lua
@@ -0,0 +1,12 @@
+--!/usr/bin/lua
+local M = {}
+
+local feed_list = {}
+
+function M.list()
+  
+  return feed_list
+
+end
+
+return M
--- lua/rss/viewer.lua
+++ lua/rss/viewer.lua
@@ -3,9 +3,36 @@ local M = {}
 local bufnr = -1
 local winid = -1
 
+local fd = require('rss.feed')
+
+
+local function update_context()
+  local text = {'RSS list:'}
+
+  for _, v in ipairs(fd.list()) do
+  end
+  vim.api.nvim_buf_set_option(bufnr, 'modifiable', true)
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, text)
+  vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+end
+
 
 function M.open_win()
-  
+  if bufnr ~= -1 and vim.api.nvim_buf_is_valid(bufnr) then
+    vim.api.nvim_buf_delete(bufnr, {
+      force = true,
+      unload = false,
+    })
+  end
+  vim.api.nvim_command('topleft vsplit __rss_viwer__')
+  local lines = vim.o.columns * 30 / 100
+  vim.api.nvim_command('vertical resize ' .. tostring(lines))
+  vim.api.nvim_command(
+    'setlocal buftype=nofile bufhidden=wipe nobuflisted nolist noswapfile nowrap cursorline nospell nonu norelativenumber winfixheight nomodifiable  winfixwidth'
+  )
+  vim.api.nvim_command('set filetype=RSSViewer')
+  bufnr = vim.api.nvim_get_current_buf()
+  update_context()
 end
 
 
