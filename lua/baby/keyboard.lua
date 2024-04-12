7f102fd7b6e815795b0331bfa9a915fcdabfa9a0
Eric Wong <eric@wsdjeg.net>
2024-04-12 19:49:22 +0800

    update

--- lua/baby/keyboard.lua
+++ lua/baby/keyboard.lua
@@ -7,6 +7,7 @@ local v = require('spacevim.api').import('vim')
 
 local bufnr = -1
 local winnr = -1
+local speed = 0
 
 local function insert_box(w)
   local box_top = "+--------------------------------------+"
@@ -39,21 +40,27 @@ end
 function M.start()
   winnr = open_win()
   local char
+  local status = string.format('                 按键速度 %s/分钟     ', speed)
   local orgtext = 'The next error mapping and the error transient state can be used to browse errors from syntax checkers'
   local context = ""
   local wait_for_input = true
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { orgtext, context })
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {status, orgtext, context })
   vim.cmd.redraw()
+  local n = 0
+  local t = os.time()
   while wait_for_input do
     char = v.getchar()
+    n = n + 1
+    speed = math.floor(n / (os.difftime(os.time(), t)) * 60)
+    status = string.format('                 按键速度 %s/分钟     ', speed)
     if char == K.t('<Esc>') then
       wait_for_input = false
     elseif char == K.t('<Bs>') then
       context = vim.fn.substitute(context, '.$', '', 'g')
-      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { orgtext, context })
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {status, orgtext, context })
     else
       context = context .. char
-      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { orgtext, context })
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {status, orgtext, context })
     end
     vim.cmd.redraw()
   end
