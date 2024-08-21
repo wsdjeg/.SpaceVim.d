2772ac95e197a20bf3210e8e4bbb8d0e8784ad53
Eric Wong <eric@wsdjeg.net>
2024-08-21 13:36:21 +0800

    update

--- lua/fuzzy/windows.lua
+++ lua/fuzzy/windows.lua
@@ -1,5 +1,16 @@
 local w = {}
 
+local prompt = require('spacevim.api.prompt')
+
+-- open default windows
+
+local bufnr = -1
+local winid = -1
+
+local function open_result_win()
+  
+end
+
 function w.open(input, files, matcher)
   
 end
